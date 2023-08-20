ff379b770de8a6b9542abcc33aee826acb912e0a
rushitgondaliya <133621234+rushitgondaliya@users.noreply.github.com>
2023-08-20 14:26:54 +0530

    commit

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 // const tokenRoutes = require("./token.route");
 const userRoute = require("./user.route");
 const categoryRoute = require("./category.route");
+const BookRoute = require("./book.route");
 
 const router = express.Router();
 
 // router.use("/token", tokenRoutes);
 router.use("/user", userRoute);
 router.use("/Category", categoryRoute);
+router.use("/Book", BookRoute);
 
 module.exports = router;
