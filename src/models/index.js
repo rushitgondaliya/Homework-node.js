aa39755ebd785de4c6026982dc975ca34ca11d80
rushitgondaliya <133621234+rushitgondaliya@users.noreply.github.com>
2023-08-20 14:08:23 +0530

    book

--- src/models/index.js
+++ src/models/index.js
@@ -1,3 +1,4 @@
+module.exports.Book=require("./Book.model");
 module.exports.category=require("./category.model");
 module.exports.product=require("./product.model");
 module.exports.User=require("./user.model");
